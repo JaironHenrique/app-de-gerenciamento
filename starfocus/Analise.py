22144d3a55fd243be0ace7c87af9fbcf41a85bc1
Amanda <nandaperez2002@gmail.com>
2021-12-02 19:56:21 -0300

    Adicionados .csv dos dados do banco

---
+++ starfocus/Analise.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#import os
+#import sys
+import datetime
+import numpy as np
+import pandas as pd
+
+
+#Foi utilizada uma query SQLite para selecionar as tabelas 
+#do banco de dados e exportá-las como .csv
+
+atividades = pd.read_csv("./db.sqlite3_atividade.csv", encoding='utf-8')
+usuarios = pd.read_csv("./db.sqlite3_usuario.csv", encoding='utf-8')
+
+print(atividades.head(), '\n\n\n', usuarios.head())
