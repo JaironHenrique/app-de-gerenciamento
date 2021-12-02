b9768af025f6db5f41e71a051babe6489a2e689c
EzequielEBS <ezequielsantos2306@gmail.com>
2021-12-02 18:41:26 -0300

    Alterei algumas funções e adicionei usuario novamente

--- starfocus/atividades/urls.py
+++ starfocus/atividades/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from atividades import views
 
 urlpatterns = [
-    path("registrar/", views.registrar, name="registrar"),
-    path("index/", views.index, name="index"),
-    path("registrar_user/", views.registrar_user, name="registrar_user"),
-    path("index_2/", views.index_2, name="index_2")
+    # path("registrar/", views.registrar, name="registrar"),
+    # path("registrar_user/", views.registrar_user, name="registrar_user"),
+    path("mostrar_atvs/", views.mostrar_atvs, name="mostrar_atvs"),
+    path("mostrar_users/", views.mostrar_users, name="mostrar_users")
 ]
--- starfocus/atividades/views.py
+++ starfocus/atividades/views.py
@@ -2,50 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from atividades.models import Atividade, Usuario
-import random
-from datetime import datetime, timedelta
-import numpy as np
-from faker import Faker
+# import random
+# from datetime import datetime, timedelta
+# import numpy as np
+# from faker import Faker
 
-# Create your views here.
-
-def index(request):
+def mostrar_atvs(request):
     msg = ""
     lista_atv = Atividade.objects.all()
     for atv in lista_atv:
         msg += str(atv)+"<br>"
     return HttpResponse(msg)
 
-def index_2(request):
+def mostrar_users(request):
     msg = ""
     lista_user = Usuario.objects.all()
     for user in lista_user:
         msg += str(user)+"<br>"
     return HttpResponse(msg)
 
-def registrar(request):
-    atividades = {"Estudar Cálculo":"Descrição_1", "Estudar Linguagens de Programação":"Descrição_2",
-                  "Praticar exercícios físicos":"Descrição_3","Estudar Economia":"Descrição_4",
-                  "Fazer atividades":"Descrição_5"}
-    lista_atividades = list(atividades.keys())
-    datas = np.arange(np.datetime64('2021-01-01'), np.datetime64('2021-12-31'))
-    datas = datas.tolist()
+# def registrar(request):
+#     atividades = {"Estudar Cálculo":"Descrição_1", "Estudar Linguagens de Programação":"Descrição_2",
+#                   "Praticar exercícios físicos":"Descrição_3","Estudar Economia":"Descrição_4",
+#                   "Fazer atividades":"Descrição_5"}
+#     lista_atividades = list(atividades.keys())
+#     datas = np.arange(np.datetime64('2021-01-01'), np.datetime64('2021-12-31'))
+#     datas = datas.tolist()
     
-    for i in range(200):
-        duracao = random.randint(1,10)
-        nome = random.choice(lista_atividades)
-        descricao = atividades[nome]
-        data = random.choice(datas)
-        atividade = Atividade(nome=nome, descricao=descricao, duracao=duracao, data=data)
-        atividade.save()
+#     for i in range(200):
+#         duracao = random.randint(1,10)
+#         nome = random.choice(lista_atividades)
+#         descricao = atividades[nome]
+#         data = random.choice(datas)
+#         atividade = Atividade(nome=nome, descricao=descricao, duracao=duracao, data=data)
+#         atividade.save()
     
-    return HttpResponse("Nome: " + nome + " - Descrição: " + str(descricao) + " - Duração: " + str(duracao) + " - Data: " + str(data))
+#     return HttpResponse("Nome: " + nome + " - Descrição: " + str(descricao) + " - Duração: " + str(duracao) + " - Data: " + str(data))
 
-def registrar_user(request):
-    faker = Faker('pt_BR')
-    for i in range(200):
-        tempo = random.randint(1,10)
-        nome = faker.name()
-        user = Usuario(nome=nome, tempo=tempo)
-        user.save()
-    return HttpResponse("Nome: " + nome + " - Tempo:" + str(tempo))
+# def registrar_user(request):
+#     faker = Faker('pt_BR')
+#     for i in range(200):
+#         tempo = random.randint(1,10)
+#         nome = faker.name()
+#         user = Usuario(nome=nome, tempo=tempo)
+#         user.save()
+#     return HttpResponse("Nome: " + nome + " - Tempo:" + str(tempo))
