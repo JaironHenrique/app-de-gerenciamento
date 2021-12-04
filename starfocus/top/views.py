5e8398c5c2d999c49ea240bca0724dc4d93b46c8
Amanda <nandaperez2002@gmail.com>
2021-12-03 21:36:05 -0300

    Adicionadas as perguntas de negócio

--- starfocus/top/views.py
+++ starfocus/top/views.py
@@ -8,6 +8,6 @@ def top_user(request, quant):
     dados = pd.read_csv(os.path.join('.','db.sqlite3_usuario.csv')).sort_values('tempo', ascending=False)
     dados.reset_index(drop=True, inplace=True)
     dados = dados.iloc[:quant]
-    mostrar = [ f"Nome: {dados.iloc[i].nome}, Tempo: {dados.iloc[i].tempo}" for i in range(quant)]
+    mostrar = [ f"Nome: {dados.iloc[i].nome} - Tempo: {dados.iloc[i].tempo} pomodoros" for i in range(quant)]
     return render(request, 'top/index.html', 
                   context={'mostrar': mostrar})
